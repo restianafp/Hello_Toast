05d4e9e51c0e6b298d86bcd34b8dbf17e9a3e62f
restianafp <restianafp@gmail.com>
2020-09-21 11:43:41 +0700

    RelativeLayout

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView showCount;
+    int mCount=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showCount=findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+
+    public void showCount(View view) {
+        mCount++;
+        if (showCount!=null){
+            showCount.setText(mCount+"");
+        }
+
+    }
+}
